a4cbc378001aaa8be5777e2f9892d8bdb03702ae
PaolaAndreaGA <paga0209@gmial.com>
2022-02-11 07:31:01 -0500

    1-hbnb.js

--- static/scripts/1-hbnb.js
+++ static/scripts/1-hbnb.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-    let amenityChecked = {};
-    $(document).on('change', ".checkbox-amenity", function () {
-        if($(this).is(':checked')){
-                amenityChecked[$(this).data('id')] = $(this).data('name');
-            } else {
-                delete amenityChecked[$(this).data('id')];
-            }
-        
-            let dictAmenity = Object.values(amenityChecked);
-            if (dictAmenity.length > 0) {
-                $('.select').text(dictAmenity.join(', '));
-            } else {
-                $('.select').html('&nbsp;');
-            }
-        });
+  const amenityChecked = {};
+  $(document).on('change', '.checkbox-amenity', function () {
+    if ($(this).is(':checked')) {
+      amenityChecked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenityChecked[$(this).data('id')];
+    }
+
+    const dictAmenity = Object.values(amenityChecked);
+    if (dictAmenity.length > 0) {
+      $('.select').text(dictAmenity.join(', '));
+    } else {
+      $('.select').html('&nbsp;');
+    }
+  });
 });
