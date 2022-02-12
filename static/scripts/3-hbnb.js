fa11cf7fdf78b15c3394e7feb6b98c3f78c89deb
PaolaAndreaGA <paga0209@gmial.com>
2022-02-11 21:01:04 -0500

    [UPDATE] Fixed semistandard  3-hbnb.js

--- static/scripts/3-hbnb.js
+++ static/scripts/3-hbnb.js
@@ -32,27 +32,27 @@ $.ajax({
   contentType: 'application/json',
   success: function (data) {
     for (let i = 0; i < data.length; i++) {
-      let info = data[i];
-      let numberGuest = "";
-      let numberRooms = "";
-      let numberBath = "";
-      if (info.max_guest != 1) {
-        numberGuest = "Guests"
-      } else { 
-        numberGuest = "Guest"
-      };
-      if (info.number_rooms != 1) {
-        numberRooms = "Rooms"
-      } else { 
-        numberRooms = "Room"
-      };
-      if (info.number_bathrooms!= 1) {
-        numberBath = "Bathrooms"
-      } else { 
-        numberBath = "Bathroom"
-      };
+      const info = data[i];
+      let numberGuest = '';
+      let numberRooms = '';
+      let numberBath = '';
+      if (info.max_guest !== 1) {
+        numberGuest = 'Guests';
+      } else {
+        numberGuest = 'Guest';
+      }
+      if (info.number_rooms !== 1) {
+        numberRooms = 'Rooms';
+      } else {
+        numberRooms = 'Room';
+      }
+      if (info.number_bathrooms !== 1) {
+        numberBath = 'Bathrooms';
+      } else {
+        numberBath = 'Bathroom';
+      }
 
-      $('.places').append('<article><div class="title_box"><h2>'+ info.name +'</h2><div class="price_by_night"><p>$' + info.price_by_night + '</p></div></div><div class="information"><div class="max_guest"><p>' + info.max_guest + '</p><p>' + numberGuest + '</p></div><div class="number_rooms"><p>' + info.number_rooms + '</p><p>' + numberRooms + '</p></div><div class="number_bathrooms"><p>' + info.number_bathrooms + '</p><p>' + numberBath + '</p></div></div><div class="user"><b>Owner: </b> Paola & Diana </div>' + '<div class="description"><p>' + info.description + '</p></div></article>');
+      $('.places').append('<article><div class="title_box"><h2>' + info.name + '</h2><div class="price_by_night"><p>$' + info.price_by_night + '</p></div></div><div class="information"><div class="max_guest"><p>' + info.max_guest + '</p><p>' + numberGuest + '</p></div><div class="number_rooms"><p>' + info.number_rooms + '</p><p>' + numberRooms + '</p></div><div class="number_bathrooms"><p>' + info.number_bathrooms + '</p><p>' + numberBath + '</p></div></div><div class="user"><b>Owner: </b> Paola & Diana </div>' + '<div class="description"><p>' + info.description + '</p></div></article>');
     }
   }
 });
